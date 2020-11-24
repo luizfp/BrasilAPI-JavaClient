651dc0d3bb86ec8c605b13719ca89bc841cbe331
Luiz Felipe <luiz.felipe_95@hotmail.com>
2020-11-24 18:25:21 -0300

    test: cria teste para o ServiceLocator

---
+++ src/test/java/br/com/brasilapi/javaclient/network/ServiceLocatorTest.java
@@ -0,0 +1,43 @@
+package br.com.brasilapi.javaclient.network;
+
+import br.com.brasilapi.javaclient.BrasilApiConfig;
+import br.com.brasilapi.javaclient.Injection;
+import br.com.brasilapi.javaclient.cep.CepService;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+
+import java.time.Duration;
+
+import static com.google.common.truth.Truth.assertThat;
+
+/**
+ * Created on 2020-11-24
+ *
+ * @author Luiz Felipe (https://github.com/luizfp)
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public class ServiceLocatorTest {
+    private ServiceLocator serviceLocator;
+
+    @BeforeAll
+    public void beforeAll() {
+        this.serviceLocator = Injection.provideServiceLocator(BrasilApiConfig.of(Duration.ZERO));
+    }
+
+    @Test
+    public void givenAnExistingServiceThenLocatorShouldFind() {
+        final CepService service = serviceLocator.getService(CepService.class);
+        assertThat(service).isNotNull();
+    }
+
+    @Test
+    public void givenANoRealServiceThenLocatorShouldFail() {
+        Assertions.assertThrows(IllegalArgumentException.class, () -> serviceLocator.getService(NotAService.class));
+    }
+
+    private static final class NotAService {
+
+    }
+}
